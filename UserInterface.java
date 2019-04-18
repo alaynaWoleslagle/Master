b1d697c2f9561726c419b245eac7bba8e3c2ba34
Alayna Woleslagle <AlaynaWoleslagle@Jodis-MacBook-Air.local>
2019-04-18 19:15:17 -0400

    updated UI - added hallway and room classes

--- UserInterface.java
+++ UserInterface.java
@@ -1,11 +1,13 @@
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Line;
-import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 
@@ -19,14 +21,16 @@ public class UserInterface extends Application{
 	@Override
 	public void start(Stage stage) throws Exception {
 		
-		Group root = new Group();
+		//Group root = new Group();
 		
-		setUpGameBoard(root);
+		Scene startScene = createStartScreen();
+		
+		// call this after character selection
+		//createGameBoard(root);
 		
 		Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
 		
-		Scene scene = new Scene(root, 0, 0);
-		stage.setScene(scene);
+		stage.setScene(startScene);
 		
 		stage.setX(primaryScreenBounds.getMinX());
         stage.setY(primaryScreenBounds.getMinY());
@@ -39,43 +43,23 @@ public class UserInterface extends Application{
 		
 	}
 	
-	private void setUpGameBoard(Group root) {
+	private void createGameBoard(Group root) {
 		
-		// Build vertical hallways
-        for (int x = 0 ; x < 500 ; x+=200) {
-            for (int y = 0 ; y < 400 ; y+=200) {
-                Line line1 = new Line(); 
-                line1.setStartX(x + 35); 
-                line1.setStartY(y); 
-                line1.setEndX(x + 35); 
-                line1.setEndY(y + 200);
-                
-                Line line2 = new Line(); 
-                line2.setStartX(x + 60); 
-                line2.setStartY(y); 
-                line2.setEndX(x + 60); 
-                line2.setEndY(y + 200);
-                
-                root.getChildren().addAll(line1, line2);
-            }
-        }
-        
 		// Build horizontal hallways
         for (int x = 0 ; x < 400 ; x+=200) {
             for (int y = 0 ; y < 500 ; y+=200) {
-                Line line1 = new Line(); 
-                line1.setStartX(x); 
-                line1.setStartY(y + 35); 
-                line1.setEndX(x + 200); 
-                line1.setEndY(y + 35);
+            		Hallway hallway = new Hallway(x + 35, y, "horizontal");
                 
-                Line line2 = new Line(); 
-                line2.setStartX(x); 
-                line2.setStartY(y + 60); 
-                line2.setEndX(x + 200); 
-                line2.setEndY(y + 60);
+                root.getChildren().addAll(hallway.getHallway());
+            }
+        }
+        
+		// Build vertical hallways
+        for (int x = 0 ; x < 500 ; x+=200) {
+            for (int y = 0 ; y < 400 ; y+=200) {
+            		Hallway hallway = new Hallway(x, y + 60, "vertical");
                 
-                root.getChildren().addAll(line1, line2);
+                root.getChildren().addAll(hallway.getHallway());
             }
         }
         
@@ -86,20 +70,43 @@ public class UserInterface extends Application{
 		
         for (int x = 0 ; x < 600 ; x+=200) {
             for (int y = 0 ; y < 600 ; y+=200) {
-                Rectangle square = new Rectangle(x, y, 100, 100);
-                Text roomName = new Text(roomNames[i]);
+            		Room room = new Room(x, y, roomNames[i]);
+                Text roomName = new Text(room.getName());
                 roomName.setTranslateY(y + 50);
                 roomName.setTranslateX(x + 10);
-                square.setFill(Color.WHITE);
-                square.setStroke(Color.BLACK);
-                root.getChildren().addAll(square, roomName);
+                root.getChildren().addAll(room.getRoom(), roomName);
+                
                 ++i;
             }
         }
         
+        // create rooms
+        // create hallways and pass in connecting rooms
+        // manually assign hallways for each room
+        
         // shift board slightly, so it isn't right on the left edge of window
         root.setTranslateX(root.getTranslateX() + 50);
         root.setTranslateY(root.getTranslateY() + 50);
+	}
+	
+	private Scene createStartScreen()
+	{
+		StackPane root = new StackPane();
+        Text welcomeText = new Text("Welcome to Clue-Less!");
+        welcomeText.setStyle("-fx-font: 48 arial;");
+        welcomeText.setTextAlignment(TextAlignment.CENTER);
+        Button joinButton = new Button("Join Game");
+        Button startButton = new Button("Start a New Game");
+        root.getChildren().addAll(welcomeText, joinButton, startButton);
+        StackPane.setAlignment(welcomeText, Pos.CENTER);
+        joinButton.setMinWidth(150);
+        startButton.setMinWidth(150);
+        joinButton.setTranslateY(welcomeText.getTranslateY() + 100);
+        startButton.setTranslateY(welcomeText.getTranslateY() + 150);
+        
+		Scene startScene = new Scene(root, 0, 0);
+		return startScene;
+		
 	}
 
 }
